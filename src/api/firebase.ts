c965c5283b11cee22c818dcb3305f453ac4bab9c
langat <kipngenolangat36@gmail.com>
2025-03-25 14:01:33 +0300

    update to remove .env configuration

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -2,22 +2,23 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { config } from 'dotenv';
+
+config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCojT3VZVXZCU4cTh4tUjCKsnaVJOX8zM4",
-  authDomain: "soccermls-618b0.firebaseapp.com",
-  projectId: "soccermls-618b0",
-  storageBucket: "soccermls-618b0.firebasestorage.app",
-  messagingSenderId: "10448729286",
-  appId: "1:10448729286:web:928cfce4e907ad8f6053c7",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-
-
 
 export const signUp = (email: string, password: string) =>
   createUserWithEmailAndPassword(auth, email, password);
